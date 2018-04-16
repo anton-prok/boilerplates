3347a187cac56e34006adc31cc79152d7b3fb8d5
Anton Prokopenko <anton.9090@bk.ru>
2018-04-16 23:29:50 +0300

    add base config to work

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # devbp_2
 gulp boilerplate for markup &amp; some js
+
+
+1. npm i
+2. gulp
+3. ... 
+4. profit!
+
+5. gulp build
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var autoprefixer = require('gulp-autoprefixer');
 var pug = require('gulp-pug');
 var browserSync = require('browser-sync').create();
 var cssnano = require('gulp-cssnano');
+var del = require('del');
+var tiny = require('gulp-tinypng-nokey');
+
 
 gulp.task('pug', function buildHTML() {
     return gulp.src('src/assets/pug/pages/*.pug')
@@ -12,10 +15,11 @@ gulp.task('pug', function buildHTML() {
                 pretty: true
             }))
             .pipe(gulp.dest('./src'))
+            .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('sass', function () {
-    return gulp.src('src/assets/sass/**/*.sass')
+    return gulp.src('src/assets/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
@@ -23,7 +27,8 @@ gulp.task('sass', function () {
             grid: true
         }))
         .pipe(cssnano())
-        .pipe(gulp.dest('src/assets/css'));
+        .pipe(gulp.dest('src/assets/css'))
+        .pipe(browserSync.reload({stream: true}));
 });  
  
 gulp.task('webserver', function() {
@@ -31,11 +36,39 @@ gulp.task('webserver', function() {
         server: {
             baseDir: "./src"
         },
-        port: 8080
+        port: 8080,
+        notify: false
     });
 });
+
+gulp.task('watch', function() {
+    gulp.run('pug:watch')
+    gulp.run('sass:watch')
+})
+
+gulp.task('pug:watch', function() {
+    gulp.watch('src/assets/pug/**/*.pug', ['pug'])
+})
+
+gulp.task('sass:watch', function() {
+    gulp.watch('src/assets/scss/**/*.scss', ['sass'])
+})
 
 gulp.task('default', function() {
     gulp.run('pug')
     gulp.run('sass')
+    gulp.run('webserver')
+    gulp.run('watch')
+})
+
+gulp.task("delete", ['delete'], function() {
+    return del("build");
+});
+
+gulp.task('build', function() {
+    gulp.src('src/**/*.html').pipe(gulp.dest("build/"));  
+    gulp.src('src/assets/css/**/*.css').pipe(gulp.dest("build/assets/css"));
+    gulp.src('src/assets/img/**/*.{png,jpg,gif}')
+        .pipe(tiny())
+        .pipe(gulp.dest("build/assets/img"));
 })
