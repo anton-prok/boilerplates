420a3735a5663e415db40dfa4e63fb4e92a7e648
Anton <anton.9090@bk.ru>
2021-03-26 23:29:16 +0300

    ...

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,37 +1,36 @@
-var gulp = require('gulp');
+const { src, dest, watch, series, task } = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var pug = require('gulp-pug');
+var gulpPug = require('gulp-pug');
 var browserSync = require('browser-sync').create();
 var cssnano = require('gulp-cssnano');
 var del = require('del');
 var tiny = require('gulp-tinypng-nokey');
 
 
-gulp.task('pug', function buildHTML() {
-    return gulp.src('src/assets/pug/pages/*.pug')
-            .pipe(pug({
-                basedir: 'src/assets/pug/pages',
+function buildHTML() {
+    return src('src/pug/pages/*.pug')
+            .pipe(gulpPug({
+                basedir: 'src/pug/pages',
                 pretty: true
             }))
-            .pipe(gulp.dest('./src'))
-            .pipe(browserSync.reload({stream: true}))
-});
+            .pipe(dest('./src'))
+            .pipe(browserSync.stream({ stream: true  }))
+}
 
-gulp.task('sass', function () {
-    return gulp.src('src/assets/scss/**/*.scss')
+function gulpSass() {
+    return src('src/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+            browsers: ['last 6 versions'],
             cascade: false,
             grid: true
         }))
-        .pipe(cssnano())
-        .pipe(gulp.dest('src/assets/css'))
-        .pipe(browserSync.reload({stream: true}));
-});  
+        .pipe(dest('src/css'))
+        .pipe(browserSync.reload({ stream: true }));
+}
  
-gulp.task('webserver', function() {
+function server() {
     browserSync.init({
         server: {
             baseDir: "./src"
@@ -39,36 +38,24 @@ gulp.task('webserver', function() {
         port: 8080,
         notify: false
     });
-});
+    watch(['src/pug/pages/*.pug'], { events: 'change' }, buildHTML).on('change', function() {
+        browserSync.reload({ match: "**/*.html" });
+    });
+    watch(['src/scss/**/*.scss'], { events: 'change' }, gulpSass).on('change', function() {
+        browserSync.reload({ match: "**/*.css" });
+    });
+}
 
-gulp.task('watch', function() {
-    gulp.run('pug:watch')
-    gulp.run('sass:watch')
-})
-
-gulp.task('pug:watch', function() {
-    gulp.watch('src/assets/pug/**/*.pug', ['pug'])
-})
-
-gulp.task('sass:watch', function() {
-    gulp.watch('src/assets/scss/**/*.scss', ['sass'])
-})
-
-gulp.task('default', function() {
-    gulp.run('pug')
-    gulp.run('sass')
-    gulp.run('webserver')
-    gulp.run('watch')
-})
-
-gulp.task("delete", ['delete'], function() {
-    return del("build");
-});
-
-gulp.task('build', function() {
-    gulp.src('src/**/*.html').pipe(gulp.dest("build/"));  
-    gulp.src('src/assets/css/**/*.css').pipe(gulp.dest("build/assets/css"));
-    gulp.src('src/assets/img/**/*.{png,jpg,gif}')
+function build(done) {
+    del.sync('../static_src/fe_build', { force: true });
+    done();
+    src('src/fonts/**/*.{woff,css}').pipe(dest("../static_src/fe_build/fonts"));
+    src('src/css/**/main.css').pipe(dest("../static_src/fe_build/css"));
+    src('src/js/**/*.js').pipe(dest("../static_src/fe_build/js"));
+    src('src/img/**/*.{png,jpg,gif}')
         .pipe(tiny())
-        .pipe(gulp.dest("build/assets/img"));
-})
+        .pipe(dest("../static_src/fe_build/img"));
+}
+
+exports.default = series(buildHTML, gulpSass, server);
+exports.build = series(build);
